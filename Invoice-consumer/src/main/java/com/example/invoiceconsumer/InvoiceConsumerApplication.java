ae258bb6dfd58af97eeee1a7cbeb811bd43e96e1
vahid shahbazi <vahidsh1@gmail.com>
2023-01-03 09:53:23 +0330

    add invoice consumer service

---
+++ Invoice-consumer/src/main/java/com/example/invoiceconsumer/InvoiceConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.example.invoiceconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceConsumerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(InvoiceConsumerApplication.class, args);
+	}
+
+}
